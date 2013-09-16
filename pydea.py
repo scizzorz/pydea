580802357276787aa0f8c317babd04a28af50b18
John Weachock <jweachock@gmail.com>
2013-09-16 13:58:46 -0400

    Update to bumpy

--- pydea.py
+++ pydea.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import bumpy
 import os
 import sys
 
@@ -17,29 +18,48 @@ class Idea:
 		self.name = os.path.splitext(filename)[0]
 		self.source = open(filename).read()
 
-def main(args):
-	stream = Stream()
-	if len(args) == 1:
-		if args[0] == 'init':
-			if stream.exists:
-				print 'Already a pydea stream'
-				return
+	def __repr__(self):
+		return '[Idea: {}]'.format(self.name)
 
-			with open('.pydea', 'w+') as temp:
-				temp.write('* title = Testing\n')
-			print 'Created empty pydea stream'
-		elif args[0] == 'view':
-			if not stream.exists:
-				print 'Not a pydea stream'
-				return
+@bumpy.task
+def init():
+	'''Initialize a Pydea stream'''
+	if stream.exists: bumpy.abort('Already a Pydea stream')
 
-			print 'Finding pydeas...'
+	with open('.pydea', 'w+') as temp:
+		temp.write('* title = Testing\n')
+	print 'Created empty Pydea stream'
+
+@bumpy.task
+def show():
+	'''View the Pydea stream'''
+	if not stream.exists: bumpy.abort('Not a Pydea stream')
+
+	print stream.ideas
+
+@bumpy.task
+def add(*args):
+	'''Add a new Pydea'''
+	if not stream.exists: bumpy.abort('Not a Pydea stream')
+
+@bumpy.default
+@bumpy.private
+def default(*args):
+	'''Add items to the Pydea stream or display it'''
+	if not stream.exists: bumpy.abort('Not a Pydea stream')
+
+	if args:
+		add(*args)
 	else:
-		if not stream.exists:
-			print 'Not a pydea stream'
-			return
+		show()
 
-		print 'Adding pydea...'
+@bumpy.setup
+@bumpy.private
+def setup():
+	global stream
+	stream = Stream()
 
 if __name__ == '__main__':
-	main(sys.argv[1:])
+	suppress = [key for key in bumpy.LOCALE if not key.startswith('abort')]
+	bumpy.config(cli = True, suppress = suppress)
+	bumpy.main(sys.argv[1:])
